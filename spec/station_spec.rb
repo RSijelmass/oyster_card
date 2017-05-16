577f3eeed25fc9a3dd106fd8603a849ac291809b
bannastre <bannastre@gmail.com>
2017-05-16 18:52:43 +0100

    Initialize Station class with attr_reader - initialize with name, zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,4 +2,14 @@ require 'station'
 
 describe Station do
 
+  subject(:station) { described_class.new(:name, :zone) }
+
+    it { is_expected.to respond_to(:name) }
+
+    it { is_expected.to respond_to(:zone) }
+
+    it "calls station.name" do
+      expect(!!station.name).to eq true
+    end
+
 end
