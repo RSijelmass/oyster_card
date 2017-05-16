d21ccb6a8c29ee92c8ff931af7f53746abd7acb4
bannastre <bannastre@gmail.com>
2017-05-16 15:59:22 +0100

    Refactor, remove @in_journey instance variable from Oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,7 +8,6 @@ class Oystercard
 
   def initialize
     @balance = 0
-    @in_journey = false
     @entry_station = nil
   end
 
@@ -18,18 +17,16 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
 
   def touch_in(station)
     raise "Balance below minimum" if @balance < Fare::MIN_FARE
-    @in_journey = true
     @entry_station = station
   end
 
   def touch_out
     deduct_fare(Fare::MIN_FARE)
-    @in_journey = false
     @entry_station = nil
   end
 
