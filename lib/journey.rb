9631b0c7c2c419d626ef7122ce57e5862e8ecb4d
“rogrenke” <roland@rolandgrenke.com>
2017-05-17 14:50:29 +0100

    Initialize Journey class with entry_station attribute - has default value of :no_station - create attr_reader for the attribute

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,2 +1,8 @@
 class Journey
+  attr_reader :entry_station
+
+  def initialize(entry_station = :no_station)
+    @entry_station = entry_station
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,12 @@
 require 'journey'
 
 describe Journey do
+  subject(:journey) { described_class.new }
+
+  it "has an entry_station" do
+    journey1 = Journey.new("Brixton")
+    expect(journey1.entry_station).to eq "Brixton"
+  end
+
 
 end
