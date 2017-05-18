3e671e5459f80c484a015ec64a3f2f75f6320fbd
“rogrenke” <roland@rolandgrenke.com>
2017-05-18 16:26:12 +0100

    Adds @started instance variable in JourneyLog class - updates #touch_out to create new journey if user forgot to touch_in

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -4,10 +4,18 @@ class JourneyLog
 
   def initialize
     @journeys = []
+    @started = false
   end
 
   def start(station)
     @journeys << Journey.new.start_journey(station)
+    @started = true
+  end
+
+  def finish(station)
+    start(:no_station) unless @started
+    @journeys[-1].end_journey(station)
+    @started = false
   end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,14 +2,27 @@ require 'journey_log'
 
 describe JourneyLog do
   subject(:journeylog) { described_class.new }
-  let(:journey) { double(:journey, start_journey: true) }
+  let(:journey) { double(:journey) }
   let(:station) { double(:station) }
-  describe '#start' do
+  let(:station2) { double(:station2) }
 
+  describe '#start' do
     it 'starts a new journey' do
       expect{journeylog.start(station)}.to change{ journeylog.journeys.length }.by 1
     end
+  end
 
+  describe '#finish' do
+    it 'finishes a current journey' do
+      # not using proper doubles
+      journeylog.start(station)
+      journeylog.finish(station2)
+      expect(journeylog.journeys[-1].exit_station).to eq station2
+    end
+
+    it 'creates a new journey in case we only touch out' do
+      expect{journeylog.finish(station)}.to change{ journeylog.journeys.length }.by 1
+    end
   end
 
 end
