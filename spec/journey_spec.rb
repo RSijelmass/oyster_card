cfc9c6d08ef44d45946028dfa80fbbcbc267edd4
Michael Jacobson <michael.jacobson89@gmail.com>
2017-05-19 11:58:51 +0100

    • refactored unit tests • all tests now passing

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,13 +4,13 @@ describe Journey do
   subject(:journey) { described_class.new }
 
   it 'has an entry_station' do
-    journey1 = Journey.new('Brixton')
+    journey1 = Journey.new(entry_station:'Brixton')
     expect(journey1.entry_station).to eq 'Brixton'
   end
 
   it 'has an exit_station' do
-    journey1 = Journey.new('Brixton','Liverpool Street')
-    expect(journey1.exit_station).to eq 'Liverpool Street'
+    journey1 = Journey.new(exit_station: 'Brixton', entry_station: 'Liverpool Street')
+    expect(journey1.exit_station).to eq 'Brixton'
   end
 
   it 'knows when it has been completed' do
